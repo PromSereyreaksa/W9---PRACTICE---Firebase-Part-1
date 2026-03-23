f4ed0f2f0c6210d8316e08bbb282de212b39b71a
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-03-23 22:23:39 +0700

    W9-01 – Fetch and display songs in the library screen

---
+++ data/dtos/song_dto.dart
@@ -0,0 +1,34 @@
+import '../../model/songs/song.dart';
+
+class SongDto {
+  static const String idKey = 'id';
+  static const String titleKey = 'title';
+  static const String artistKey = 'artistId';
+  static const String durationKey = 'duration';
+  static const String imageUrlKey = 'imageUrl';
+
+  static Song fromJson(String id, Map<String, dynamic> json) {
+    assert(json[titleKey] is String);
+    assert(json[artistKey] is String);
+    assert(json[durationKey] is int);
+    assert(json[imageUrlKey] is String);
+
+    return Song(
+      id: id,
+      title: json[titleKey],
+      artistId: json[artistKey],
+      duration: Duration(milliseconds: json[durationKey]),
+      imageUrl: Uri.parse(json[imageUrlKey]),
+    );
+  }
+
+  Map<String, dynamic> toJson(Song song) {
+    return {
+      idKey: song.id,
+      titleKey: song.title,
+      artistKey: song.artistId,
+      durationKey: song.duration.inMilliseconds,
+      imageUrlKey: song.imageUrl.toString(),
+    };
+  }
+}
---
+++ ui/widgets/song/song_tile.dart
@@ -0,0 +1,41 @@
+import 'package:blabla/FIREBASE/data/dtos/song_dto.dart';
+import 'package:flutter/material.dart';
+
+import '../../../model/songs/song.dart';
+
+class SongTile extends StatelessWidget {
+  const SongTile({
+    super.key,
+    required this.song,
+    required this.isPlaying,
+    required this.onTap,
+  });
+
+  final Song song;
+  final bool isPlaying;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: ListTile(
+          onTap: onTap,
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(song.imageUrl.toString()),
+          ),
+          title: Text(song.title),
+          trailing: Text(
+            isPlaying ? "Playing" : "",
+            style: TextStyle(color: Colors.amber),
+          ),
+        ),
+      ),
+    );
+  }
+}
